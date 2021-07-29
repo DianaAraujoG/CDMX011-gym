45e2bf9800854420ba6bd9258ec80e9f2ecbd91d
DianaAraujoG <dianac251@gmail.com>
2021-07-29 09:40:16 -0500

    Hola

---
+++ session-01/exercises/star-lovers/solutions/stars.js
@@ -0,0 +1,17 @@
+export const printStars = (numStar) =>{
+    let Stars = ''
+    for(let i= 1; i<=numStar; i++){
+        Stars += '*'
+    }
+    console.log(Starts)
+    return Stars
+}
+
+export const printStarsChage = (numStar) =>{
+    let Stars = []
+    for(let i= 1; i<=numStar; i++){
+        Stars += '*'
+    }
+    console.log(Starts)
+    return Stars
+}
